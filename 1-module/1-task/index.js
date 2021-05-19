fd95d2e2c5f234228831db17a3ffafe7f3bfeec9
Ruslan Zaripov <rus.zar20@gmail.com>
2021-05-20 00:12:50 +0300

    FirstModule.Tasks1-5

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,12 @@
-function factorial(n) {
-  // ваш код...
-}
+  function factorial(n) {
+    
+    let result = 1;
+    
+    for (let i = 1; i <= n; i++) {
+      result = result * i;
+    }
+    return result;
+  }
+  
+  alert (factorial(5));
+  alert (factorial(3));
