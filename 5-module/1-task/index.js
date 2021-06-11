3a97037bbb07ba27f26d09dc6c0bb2ec61692678
Ruslan Zaripov <rus.zar20@gmail.com>
2021-06-11 18:32:51 +0300

    Module 4, 5 Tasks fixed 5-1

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,5 +1,4 @@
 function hideSelf() {
   let button = document.querySelector(".hide-self-button");
   button.addEventListener("click", () => button.setAttribute("hidden", true));
-};
-hideSelf();
+}
